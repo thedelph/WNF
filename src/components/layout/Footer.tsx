0778dc0d2ffe27622e35cdc7d85196a4a260555a
thedelph <chrishide87@gmail.com>
2025-01-03 16:03:58 +0000

    Updating footer with new copyright date - testing dev/prod separation

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="footer footer-center p-4 bg-base-300 text-base-content">
       <div>
-        <p>Copyright © 2024 - Wednesday Night Football</p>
+        <p>Copyright © 2025 - Wednesday Night Football</p>
       </div>
     </footer>
   )
