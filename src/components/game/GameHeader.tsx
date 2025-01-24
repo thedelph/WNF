cbac6b3d878a83f17aa96598fa53d2a118123463
thedelph <chrishide87@gmail.com>
2025-01-24 13:16:09 +0000

    Update GameHeader.tsx

--- src/components/game/GameHeader.tsx
+++ src/components/game/GameHeader.tsx
@@ -56,8 +56,8 @@ export const GameHeader: React.FC<GameHeaderProps> = ({
       <div className="text-center space-y-2">
 
         <h1 className="text-3xl font-bold">WNF #{game.sequence_number || game.game_number || '29'}</h1>
-        <div className="text-sm text-gray-500 mb-2">Demo game - please ignore</div>
-        <div className="text-sm text-gray-500 mb-2">For testing/troubleshooting</div>
+        <div className="text-sm text-gray-500 mb-2">Register with a thumbs up 👍 in the WNF WhatsApp Group</div>
+        <div className="text-sm text-gray-500 mb-2">DM me with anyone outside of the WhatsApp group who wants to play</div>
         <div className="text-xl text-gray-600">
           <div className="flex items-center justify-center space-x-2">
             <span>{formattedDate}</span>
